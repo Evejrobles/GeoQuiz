f0f92eb753e83c1b7b3ffe88859ba887286b1e47
Eve <evejrobles@icloud.com>
2018-10-13 20:30:44 -0600

    Fixed Activity lifecycle bug.

--- app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
+++ app/src/main/java/edu/cnm/deepdive/geoquiz/QuizActivity.java
@@ -2,15 +2,21 @@ package edu.cnm.deepdive.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.security.Key;
 
 
 public class QuizActivity extends AppCompatActivity {
+
+  private static final String TAG = "QuizActivity";
+  private static final String KEY_INDEX = "index";
+
 
   private Button mTrueButton;
   private Button mFalseButton;
@@ -31,7 +37,11 @@ public class QuizActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Log.d(TAG, "onCreate(Bundle) called");
     setContentView(R.layout.activity_quiz);
+    if (savedInstanceState != null) {
+      mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+    }
 
     mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
@@ -66,10 +76,49 @@ public class QuizActivity extends AppCompatActivity {
 
   }
 
+  @Override
+  public void onStart() {
+    super.onStart();
+    Log.d(TAG, "onStart() called");
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    Log.d(TAG, "onResume() called");
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    Log.d(TAG, "onPause() called");
+  }
+
+  @Override
+  public void onSaveInstanceState(Bundle savedInstanceState) {
+    super.onSaveInstanceState(savedInstanceState);
+    Log.i(TAG, "onSaveInstanceState");
+    savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    Log.d(TAG, "onStop() called");
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    Log.d(TAG, "onDestroy() called");
+  }
+
   private void updateQuestion() {
     int question = mQuestionBank[mCurrentIndex].getTextResID();
     mQuestionTextView.setText(question);
   }
+
   private void checkAnswer(boolean userPressedTrue) {
     boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
 
@@ -77,7 +126,7 @@ public class QuizActivity extends AppCompatActivity {
 
     if (userPressedTrue == answerIsTrue) {
       messageResId = R.string.correct_toast;
-    }else {
+    } else {
       messageResId = R.string.incorrect_toast;
     }
     Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
